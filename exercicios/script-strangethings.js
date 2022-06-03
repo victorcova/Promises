c4db5c813e4831908d4b2f39c8ab14ce9a11f7cf
victorcova <victor.avoc@gmail.com>
2022-06-02 21:10:33 -0300

    Update script-strangethings.js

--- exercicios/script-strangethings.js
+++ exercicios/script-strangethings.js
@@ -3,8 +3,6 @@
 // Teste seu código chamando a função e mostrando um alerta com a frase entre aspas seguida do autor. 
 // Exemplo: "I don’t care if anyone believes me." - Joyce Byers. 
 
-// const numQuotes = 10;
-
 async function quotesStrangeThings(numQuotes){    
     try{        
         const response = await fetch (`https://strangerthings-quotes.vercel.app/api/quotes/${numQuotes}`);
