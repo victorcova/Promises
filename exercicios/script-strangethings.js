c5015cea9ed111c7d44d2dfb097fb9db0ceb9436
victorcova <victor.avoc@gmail.com>
2022-06-02 21:02:07 -0300

    exercicio completo

---
+++ exercicios/script-strangethings.js
@@ -0,0 +1,36 @@
+// Utilize a api Stranger Things Quotes disponível em https://github.com/shadowoff09/strangerthings-quotes para retornar frases aleatórias da série. 
+// Para isso, crie uma função assíncrona que consuma a API utilizando fetch.
+// Teste seu código chamando a função e mostrando um alerta com a frase entre aspas seguida do autor. 
+// Exemplo: "I don’t care if anyone believes me." - Joyce Byers. 
+
+// const numQuotes = 10;
+
+async function quotesStrangeThings(numQuotes){    
+    try{        
+        const response = await fetch (`https://strangerthings-quotes.vercel.app/api/quotes/${numQuotes}`);
+        const data = await response.json();
+
+        const quotes = data.map(function (quoteAuthor) { // função aqui mapeando os dados
+            const parte1 = quoteAuthor.quote;
+            const parte2 = quoteAuthor.author;
+            return `${parte1} - ${parte2}\n\n`;
+            
+            console.log(frase.substr(0,11))
+        });
+
+        console.log(`${numQuotes} Frase(s) strange Things: \n\n${quotes}`);                
+
+    } catch (error){
+        console.log("HOUVE UM ERRO: " + error.name + " - " + error.message);
+    }
+}
+
+// function quotesStrangeThings(){
+//     fetch(`https://strangerthings-quotes.vercel.app/api/quotes`)
+//         .then((response) => response.json()) // THEN para trazer a resposta
+//         .then(data => { // THEN para trazer o resultado final (data)
+//             console.log(data);
+//         }); 
+// }
+
+quotesStrangeThings(3);
--- script-async-await.js
+++ script-async-await.js
@@ -18,4 +18,4 @@ async function pokeTypes(){
     }
 }
 
-pokeTypes();
+// pokeTypes();
